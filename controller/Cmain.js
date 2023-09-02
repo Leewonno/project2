95f71b1feb344bf218f22f6909499b2624df3a13
Leewonno <dldnjssh123@naver.com>
2023-09-02 16:00:21 +0900

    UPDATE main page / ADD sort page / ADD 페이지별 header footer

--- controller/Cmain.js
+++ controller/Cmain.js
@@ -1,3 +1,11 @@
 exports.main = (req, res)=>{
     res.render("index");
 }
+
+exports.header = (req, res)=>{
+    res.render("header");   
+}
+
+exports.footer = (req, res)=>{
+    res.render("footer");
+}
