c84f03db8f38cf11fd6e9483184f0c8b34d87751
RainBell98 <ttiger45@naver.com>
2023-09-04 19:10:30 +0900

    [Add] User 회원가입 로직 추가

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,15 +1,42 @@
-exports.getSigninPage = (req, res)=>{
-    res.render("signin");
-}
+const models = require('../models');
+const bcrypt = require('bcrypt');
 
-exports.getSignupPage = (req, res)=>{
-    res.render("signup");
-}
+exports.getSigninPage = (req, res) => {
+  res.render('signin');
+};
 
-exports.getProfilePage = (req, res)=>{
-    res.render("mypage");
-}
+exports.getSignupPage = (req, res) => {
+  res.render('signup');
+};
 
-exports.getSortPage = (req, res)=>{
-    res.render("sort");
-}
+exports.getProfilePage = (req, res) => {
+  res.render('mypage');
+};
+
+exports.getSortPage = (req, res) => {
+  res.render('sort');
+};
+
+//암호화
+const bcryptPassword = (password) => {
+  return bcrypt.hashSync(password, saltNum);
+};
+
+//비교
+const comparePassword = (password, dbPassword) => {
+  return bcrypt.compareSync(password, dbPassword);
+};
+
+exports.postSignUp = (req, res) => {
+  models.User.create({
+    userid: req.body.userid,
+    pw: bcryptPassword(req.body.pw),
+    email: req.body.email,
+  }).then((result) => {
+    res.send({
+      userid: req.body.userid,
+      pw: bcryptPassword(req.body.pw),
+      email: req.body.email,
+    });
+  });
+};
