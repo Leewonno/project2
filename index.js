bc7d31072978abec5a4c33b2a3fae2e94ae4d348
hdh9078 <hdh9071@naver.com>
2023-09-04 20:31:46 +0900

    Feat loginPage

--- index.js
+++ index.js
@@ -44,8 +44,8 @@ app.use('/chat', (req, res) => {
   res.render('chat');
 });
 
-db.sequelize.sync({ force: true }).then( () => {
+db.sequelize.sync({ force: false }).then( () => {
     app.listen(8000, () => {
         console.log(`http://localhost:${PORT}`);
     });
-})
+});
