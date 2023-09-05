fa5ca80c4e999eb54ee9ac4e87069d024ba90da0
chxxyx <codud0826@naver.com>
2023-09-05 16:29:41 +0900

    [Style] 코드 라인 수정

--- controller/song.controller.js
+++ controller/song.controller.js
@@ -12,29 +12,16 @@ exports.insertSongByAdmin = async (req, res) => {
   try {
 
     // 곡 제목, 아티스트명, 앨범명, 가사, 작사가, 작곡가, 장르, 재생시간, 발매일, 커버 사진(url), 음원 파일(url)
-    const { title, 
-      artist, album, lyrics, 
-      writer, composer, genre, 
-      playtime, release_date,
-      song, cover } = req.body;
-    // const resData = {
-    //     title: title, 
-    //     artist: artist, 
-    //     album: album, 
-    //     lyrics: lyrics, 
-    //     writer: writer,
-    //     composer: composer, 
-    //     genre: genre, 
-    //     playtime: playtime, 
-    //     release_date: release_date, 
-    // }
+    const { title, artist, album, lyrics, 
+      writer, composer, genre, playtime, 
+      release_date, song, cover } = req.body;
 
-    const songData = await db.Song.create({title, artist, album, lyrics, writer, 
-      composer, genre, playtime, release_date, 
+    const songData = await db.Song.create({title, artist, album, 
+      lyrics, writer, composer, genre, playtime, release_date, 
       song_url: song, cover_url: cover});
     res.send(songData)
   } catch (error) {
     console.log(error)
-    res.status(500).send({message: error});
+    res.send({message: error});
   }
 }
