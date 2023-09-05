3d42a0e4d2063c84eebe506479555cb94e225d25
chxxyx <codud0826@naver.com>
2023-09-05 16:28:12 +0900

    [Feat] 곡 업로드 기능 구현 - 관리자

--- controller/song.controller.js
+++ controller/song.controller.js
@@ -1,7 +1,40 @@
+const db = require('../database/db')
+
 exports.getSongInfoPage = (req, res)=>{
     res.render("song");
 }
 
 exports.getSongUploadPage = (req, res)=>{
     res.render("song_upload");
+}
+
+exports.insertSongByAdmin = async (req, res) => {
+  try {
+
+    // 곡 제목, 아티스트명, 앨범명, 가사, 작사가, 작곡가, 장르, 재생시간, 발매일, 커버 사진(url), 음원 파일(url)
+    const { title, 
+      artist, album, lyrics, 
+      writer, composer, genre, 
+      playtime, release_date,
+      song, cover } = req.body;
+    // const resData = {
+    //     title: title, 
+    //     artist: artist, 
+    //     album: album, 
+    //     lyrics: lyrics, 
+    //     writer: writer,
+    //     composer: composer, 
+    //     genre: genre, 
+    //     playtime: playtime, 
+    //     release_date: release_date, 
+    // }
+
+    const songData = await db.Song.create({title, artist, album, lyrics, writer, 
+      composer, genre, playtime, release_date, 
+      song_url: song, cover_url: cover});
+    res.send(songData)
+  } catch (error) {
+    console.log(error)
+    res.status(500).send({message: error});
+  }
 }
