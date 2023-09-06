fd5afc2f6b5466689b142a1bc82958c4ea678726
chxxyx <codud0826@naver.com>
2023-09-06 16:23:02 +0900

    [Feat] 메인 페이지 최신 발매일 순 곡 정렬

--- router/song.router.js
+++ router/song.router.js
@@ -8,12 +8,19 @@ const upload = require('../utils/upload');
 router.get('/song', controller.getSongInfoPage); 
 router.get('/song/upload', controller.getSongUploadPage);
 
-// 기능 개발
+// 앨범 커버, 음악 파일 업로드
 router.post('/dynamic/img', upload.uploadImage, controller.uploadImg);
 router.post('/dynamic/song', upload.uploadAudio, controller.uploadSong);
 
+// 곡 정보 업로드 (관리자)
 router.post('/song/upload', controller.insertSongByAdmin);
 
+// 곡 상세 정보
 router.get('/song/detail', controller.getSongInfo);
+
+// 곡 정렬 - 1. 메인 페이지 5개씩
+router.get('/song/sort', controller.getSongBySortInMain);
+
+// 곡 정렬 - 2. 전체 곡 정렬 
 
 module.exports = router;
