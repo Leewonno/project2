5964994b26a6ff91d9e42d4c3165159c65ab0131
bliss1454 <rkgus1454@naver.com>
2023-09-06 20:56:41 +0900

    [Design] playlist 창올리기 추가

--- static/js/playbar.js
+++ static/js/playbar.js
@@ -75,3 +75,17 @@ timelineBar.addEventListener("click", function(e) {
     const {target: {value}} = e;
     audio.currentTime = value;
 })
+
+// playlist창 띄우기
+function openModal() {
+    const modal = document.querySelector('.modal_open');
+    modal.style.bottom = '0';
+    modal.removeAttribute('hidden');
+  }
+  
+  function closeModal() {
+    const modal = document.querySelector('.modal_open');
+    modal.style.bottom = '-100%';
+    modal.setAttribute('hidden', 'true');
+  }
+  
