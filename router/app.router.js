0ffb89f8fbd6fad6f5a6e73b8fdcb22f72610657
Leewonno <dldnjssh123@naver.com>
2023-09-07 15:05:49 +0900

    [ADD] 톡방 업로드 세팅

--- router/app.router.js
+++ router/app.router.js
@@ -6,4 +6,9 @@ router.get('/', controller.mainPage);
 router.get('/chat/list', controller.getChatListPage);
 router.get('/search', controller.getSearchPage);
 
+// 채팅방 만들기 임시
+router.get('/chat/upload', (req, res)=>{
+    res.render("chat_upload");
+})
+
 module.exports = router;
