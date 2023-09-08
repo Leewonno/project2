36e672c2632a35cb43cd043d7c96722c64f85f60
chxxyx <codud0826@naver.com>
2023-09-08 13:23:34 +0900

    [Feat] 메인페이지 정렬 기능 추가

--- controller/main.controller.js
+++ controller/main.controller.js
@@ -1,10 +1,82 @@
-exports.mainPage = (req, res)=>{
-    res.render("index");
-}
-exports.getChatListPage =  (req, res)=>{
-    res.render("chatlist");
-}
+const { Song } = require('../database/db');
 
-exports.getSearchPage = (req, res)=>{
-    res.render("search");
+
+exports.controller = {
+    mainPage: async (req, res) => {
+        try {
+            const limit = 6;
+            const whereClause = {};
+    
+            // 최신순으로 정렬된 데이터 가져오기
+            const recentSongs = await Song.findAndCountAll({
+                where: whereClause,
+                limit: limit,
+                order: [['release_date', 'DESC']]
+            });
+    
+            // 좋아요순으로 정렬된 데이터 가져오기
+            const likedSongs = await Song.findAndCountAll({
+                where: whereClause,
+                limit: limit,
+                order: [['like', 'DESC']]
+            });
+    
+            // 장르는 임의로 지정해줘야 함
+            const genreSongs = await Song.findAndCountAll({
+                where: { genre: '록/메탈' },
+                limit: limit,
+                order: [['release_date', 'DESC']]
+            })
+    
+            // 데이터를 객체에 추가
+            const data = {
+                recent: [],
+                like: [],
+                genre: [],
+            };
+    
+            for (const date of recentSongs.rows) {
+                data.recent.push({
+                    id: date.id,
+                    title: date.title,
+                    artist: date.artist,
+                    cover_url: date.cover_url,
+                    song_url: date.song_url
+                });
+            }
+    
+            for (const song of likedSongs.rows) {
+                data.like.push({
+                    id: song.id,
+                    title: song.title,
+                    artist: song.artist,
+                    cover_url: song.cover_url,
+                    song_url: song.song_url
+                });
+            }
+        
+            for (const song of genreSongs.rows) {
+                data.genre.push({
+                    id: song.id,
+                    title: song.title,
+                    artist: song.artist,
+                    cover_url: song.cover_url,
+                    song_url: song.song_url
+                })
+            }
+    
+            console.log(data);
+    
+            res.render("index", { data });
+        } catch (error) {
+            // 오류 처리
+        }
+    },
+    getChatListPage: (req, res) => {
+        res.render("chatlist");
+    },
+    getSearchPage: (req, res) => {
+        res.render("search");
+    }
+
 }
