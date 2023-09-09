426d2d253e7254ac5c5579bbdeafe5729a6d8efb
bliss1454 <rkgus1454@naver.com>
2023-09-09 18:36:19 +0900

    [Feat] playlist 데이터정렬

--- controller/playlist.controller.js
+++ controller/playlist.controller.js
@@ -1,3 +1,28 @@
-exports.getPlayListPage = (req, res) => {
-  res.render('playlist');
+const models = require('../database/db');
+const jwt = require("jsonwebtoken");
+
+exports.getPlayListPage = async (req, res) => {
+  try {
+    const playlists = await models.Playlist.findAll();
+    playlists.push({
+      name: req.body.pl_name,
+    })
+    res.render('playlist',{playlists});
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error retrieving playlists');
+  }
+};
+
+exports.postPlayListPage = async (req, res) => {
+  try {
+    const playlist = await models.Playlist.create({
+      name: req.body.pl_name,
+      userid: req.userid,
+    });
+    res.json({ message: 'Playlist created successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error creating playlist' });
+  }
 };
